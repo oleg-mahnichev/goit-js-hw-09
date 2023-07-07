3992db9e0d5b353ee7c251f3dab2f87c92192f4e
oleg-mahnichev <124716395+oleg-mahnichev@users.noreply.github.com>
2023-07-07 16:49:14 +0200

    made_first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+// 01-color-switcher.js
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startColorSwitcher() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorSwitcher() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(intervalId);
+}
+
+startButton.addEventListener('click', startColorSwitcher);
+stopButton.addEventListener('click', stopColorSwitcher);
 
